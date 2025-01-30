edc3c47ee1a52b6d643db6712821f7f3eb6b1409
abuabdirohman4 <abuabdirohman4@gmail.com>
2025-01-30 13:32:50 +0700

    done youtube

--- Youtube/scrape.py
+++ Youtube/scrape.py
@@ -1,12 +1,16 @@
 import pandas as pd
 from youtube_comment_downloader import *
 import json
+import time
 
 youtubeId = input("Masukkan Youtube ID: ")
 name = input("Berikan Nama Untuk File Hasil Download: ")
 
+print('Downloading Youtube comments for', youtubeId)
 downloader = YoutubeCommentDownloader()
-comments = downloader.get_comments(youtubeId)
+comments = list(downloader.get_comments(youtubeId))
+print(len(comments))
+
 with open(f"{name}.json", 'w') as f:
     for comment in comments:
         json.dump(comment, f)
@@ -14,3 +18,6 @@ with open(f"{name}.json", 'w') as f:
 
 df = pd.read_json(f"{name}.json", lines=True)
 df.to_csv(f"{name}.csv", index=False)
+
+print('\n[{:.2f} seconds] Done!'.format(time.time() - start_time))
+# y08AZxNlxnc
